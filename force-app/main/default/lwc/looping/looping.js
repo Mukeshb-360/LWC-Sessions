6dece27c6efed53a75e0b6cd7a824eaca77b4322
mukeshgitinit <mukeshb.github@gmail.com>
2022-04-25 22:06:42 +0530

    a

---
+++ force-app/main/default/lwc/looping/looping.js
@@ -0,0 +1,19 @@
+import { LightningElement } from 'lwc';
+
+export default class Looping extends LightningElement {
+    carList = ["Tata", "Audi", "Mercedes","XUV300"];
+
+    ceoList = [
+        {
+            id :1,
+            comapny : "360 Degree",
+            name : "Sundar Pichai"
+        },
+        {
+            id :2,
+            comapny : "Apple Inc",
+            name : "Cook BhaiSaa"
+        }
+
+    ]
+}
