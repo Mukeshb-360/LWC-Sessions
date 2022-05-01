5f7afd4e9452b7e3e7cb14d2d58e6be6f2e74e5c
mukeshgitinit <mukeshb.github@gmail.com>
2022-05-01 14:11:25 +0530

    typo

--- force-app/main/default/lwc/p2cSliderComponent/p2cSliderComponent.js
+++ force-app/main/default/lwc/p2cSliderComponent/p2cSliderComponent.js
@@ -7,3 +7,16 @@ export default class P2cSliderComponent extends LightningElement {
         this.val = 60 
     }
 }
+// PRINTING DATE AND TIME
+// import datetime
+// todays_date = datetime.datetime.now()
+// print(todays_date)
+// Day = todays_date.strftime('%d')
+// Month = todays_date.strftime('%m')
+// Year = todays_date.strftime('%Y')
+// print(Day,Month,Year)
+
+
+// o/p
+// 2022-05-01 11:13:44.664407
+// 01 05 2022
