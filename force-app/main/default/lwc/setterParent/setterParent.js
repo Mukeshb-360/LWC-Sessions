36b0a532f29c23f1a2d748b619f987e68312af24
mukeshgitinit <mukeshb.github@gmail.com>
2022-05-01 09:00:07 +0530

    toast and sattic resource demo

---
+++ force-app/main/default/lwc/setterParent/setterParent.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class SetterParent extends LightningElement {
+
+    userDetails = {
+        name : 'Rohit',
+        age : 27
+    }
+}
---
+++ force-app/main/default/lwc/toastMessagesDemo/toastMessagesDemo.js
@@ -0,0 +1,39 @@
+import { LightningElement } from 'lwc';
+
+export default class ToastMessagesDemo extends LightningElement {
+    sucessHandler(){
+        // const events = new ShowToastEvent({
+        //     title : "Success",
+        //     message : "Account Created Sucessfully !",
+        //     variant : "success"
+        // });
+        // this.dispatchEvent(events);
+        this.showToast("Success","Account Created Sucessfully !","success");
+    }
+    errorHandler(){
+        // const events = new ShowToastEvent({
+        //     title : "Error!",
+        //     message : "Account Creation Failed !",
+        //     variant : "error"
+        // });
+        // this.dispatchEvent(events);
+        this.showToast("Error!","Account Creation Failed !","error");
+    }
+    warningHandler(){
+        const events = new ShowToastEvent({
+            title : "Warning!",
+            message : "Password must conatains 1 uppercase letter !",
+            variant : "warning"
+        });
+        this.dispatchEvent(events);
+    }
+
+    showToast(title,message,variant){
+        const events = new ShowToastEvent({
+            title,
+            message,
+            variant
+        });
+        this.dispatchEvent(events);
+    }
+}
